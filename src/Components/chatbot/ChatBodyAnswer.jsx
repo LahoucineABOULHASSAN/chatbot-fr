882bafaae8e2ba64da196b367e994ce8c723e292
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2022-07-02 23:37:56 +0000

    update

--- src/Components/chatbot/ChatBodyAnswer.jsx
+++ src/Components/chatbot/ChatBodyAnswer.jsx
@@ -6,7 +6,7 @@ const ChatBodyAnswer = ({ response, time }) => {
     <div className="chat-message flex flex-start my-3">
       <ChatAvatar isAv={false} url={"/assets/chat/bot.png"} />
       <div className="flex max-w-xs items-center bg-gray-200 rounded-lg rounded-bl-none mx-2">
-        <span className="flex text-justify ml-1 h-auto text-gray-800 font-normal rounded-sm px-2 p-1 items-end">
+        <span className="flex text-justify whitespace-pre-line ml-1 h-auto text-gray-800 font-normal rounded-sm px-2 p-1 items-end">
           {response}{" "}
           <small className="text-gray-400 text-xs min-w-fit block lowercase pl-2">
             {moment(time).format("LT")}
--- src/Components/chatbot/ChatBodyQuestion.jsx
+++ src/Components/chatbot/ChatBodyQuestion.jsx
@@ -5,7 +5,7 @@ const ChatBodyQuestion = ({ ques, time }) => {
     <div className="chat-message flex flex-row-reverse flex-start">
       <ChatAvatar isAv={false} url={"/assets/chat/student.png"} />
       <div className="flex max-w-xs items-center rounded-lg rounded-br-none bg-blue-200 mx-2">
-        <span className="px-4 py-1 text-justify text-gray-600 items-end justify-center flex items-end">
+        <span className="px-4 py-1 text-justify whitespace-pre-line text-gray-600 items-end justify-center flex items-end">
           {ques}{" "}
           <small className="pl-2 lowercase min-w-fit text-left block text-right text-xs text-gray-500">
             {moment(time).format("LT")}
